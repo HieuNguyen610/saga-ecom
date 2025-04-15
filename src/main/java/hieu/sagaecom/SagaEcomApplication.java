c0a8f8b13168a25d646c5406e78d86e6ec9ca664
hieunm2 <hieunm2@vnext.vn>
2025-04-15 11:32:16 +0700

    init

---
+++ src/main/java/hieu/sagaecom/SagaEcomApplication.java
@@ -0,0 +1,13 @@
+package hieu.sagaecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaEcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/sagaecom/SagaEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.sagaecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
